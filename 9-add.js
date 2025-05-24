22a215aa40e0c0f389e82db4c5eac2e68afdc2ab
Evoless <amoogames519@gmail.com>
2025-05-24 13:48:16 +0100

    edited 9

--- 9-add.js
+++ 9-add.js
@@ -4,6 +4,7 @@ function add(a, b) {
 
 const num1 = parseInt(process.argv[2]) || 0;
 const num2 = parseInt(process.argv[3]) || 0;
-const result = add(num1, num2);
+const num3 = parseInt(process.argv[4]) || 0;
+const result = add(num1, num2 , num3);
 
 console.log(result);
