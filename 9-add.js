be94f7bfe85bad238d70a36fa84eba593c2353c1
Evoless <amoogames519@gmail.com>
2025-05-24 13:47:20 +0100

    added 9

---
+++ 9-add.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(process.argv[2]) || 0;
+const num2 = parseInt(process.argv[3]) || 0;
+const result = add(num1, num2);
+
+console.log(result);
