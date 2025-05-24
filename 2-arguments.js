462d5f458c2866feebd4d9d60854358b5b24e3c6
Evoless <amoogames519@gmail.com>
2025-05-24 12:10:00 +0100

    Modified 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,13 @@
 let argc = process.argv.slice(2);
-if (argc == 0) {
+if (argc.length == 0) {
     
     console.log("No argument");
 }
-else if(argc == 1)
+else if(argc.length == 1)
 {
     console.log("Argument found");
 }
 else
 {
-    console.log("Argument found");
+    console.log("Arguments found");
 }
